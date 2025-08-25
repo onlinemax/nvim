17633c87feeab31eab74d706b293e8157a96fae0
onlinemax <onlinepromax12@gmail.com>
2025-08-25 14:42:50 -0400

    Ensure qmlls is installed by mason-lspconfig

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -1,7 +1,7 @@
 return {
 	"mason-org/mason-lspconfig.nvim",
 	opts = {
-		ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "pyright", "svelte", "emmet_language_server" },
+		ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls", "pyright", "svelte", "emmet_language_server", "qmlls" },
 
 		automatic_enable = {
 			exclude = { "lua_ls" }
